11056564d823d9ee9b0adf82b2a2620ab6bec9ca
Daniel <technikamateur@gmx.de>
2016-06-01 00:16:47 +0200

    database improvements

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Version 1.0
+"""
+raspido Copyright (C) 2016 by Daniel Körsten aka TechnikAmateur
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import sqlite3, os, sys, time
 #create DB
 if not os.path.exists("database"):
     print("creating database...")
     os.mkdir("database")
     connection = sqlite3.connect("database/raspido.db")
-    cursor = connection.cursor()
-    sql = "CREATE TABLE streams("\
-        "id INTEGER PRIMARY KEY,"\
-        "url TEXT,"\
-        "shortname TEXT)"
-    cursor.execute(sql)
     connection.close()
     print("done")
+connection = sqlite3.connect("database/raspido.db")
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS streams(
+                  id INTEGER PRIMARY KEY,
+                  url TEXT,
+                  shortname TEXT);""")
+connection.close()
 #functions:
 def addstream(url, shortname):
     connection = sqlite3.connect("database/raspido.db")
